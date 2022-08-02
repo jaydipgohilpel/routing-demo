a6d31837761764fb1a9083704dcf2790cb67591f
PE86PD\PEPD <jaydip.gohil@proexelancers.com>
2022-08-02 11:26:49 +0530

    2-08-22  morning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path:"**",
+    component:PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
